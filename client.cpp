018c55e5b35b13fc3b36501f35c423edca900fa5
Patrick Balfanz <balfanpt1456@uwec.edu>
2020-03-03 13:05:33 -0600

    add server / client templates

---
+++ client.cpp
@@ -0,0 +1,103 @@
+// Write CPP code here
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <stdio.h>      /* for printf() and fprintf() */
+#include <sys/types.h>  /* for Socket data types */
+#include <sys/socket.h> /* for socket(), connect(), send(), and recv() */
+#include <netinet/in.h> /* for IP Socket data types */
+#include <arpa/inet.h>  /* for sockaddr_in and inet_addr() */
+#include <stdlib.h>     /* for atoi() */
+#include <string.h>     /* for memset() */
+#include <unistd.h>     /* for close() */
+#include <vector>
+#include <iostream>
+#define MAX 80
+#define PORT 9105
+#define SA struct sockaddr
+
+using namespace std;
+
+void func(int sockfd)
+{
+    string test = "Test String";
+    //Create char array buffer
+    char *testBuff = new char[4096];
+    int testLength = test.length();
+    strcpy(testBuff, test.c_str());
+    // send length
+    send(sockfd, &testLength, sizeof(testLength), 0);
+    // send message
+    send(sockfd, testBuff, testLength, 0);
+
+    cout << "Sent: " << test << endl;
+}
+
+int main()
+{
+    int sockfd, connfd;
+    struct sockaddr_in servaddr, cli;
+
+    // socket create and verification
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1)
+    {
+        printf("socket creation failed...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully created..\n");
+    bzero(&servaddr, sizeof(servaddr));
+
+    cout << "Enter a number to specify the server want to connect to: " << endl;
+    cout << "0 - thing0" << endl;
+    cout << "1 - thing1" << endl;
+    cout << "2 - thing2" << endl;
+    cout << "3 - thing3" << endl;
+    cout << "4 - localhost" << endl;
+
+    string choice;
+    string ip;
+    cin >> choice;
+    if (choice == "0")
+    {
+        ip = "10.35.195.46";
+    }
+    else if (choice == "1")
+    {
+        ip = "10.35.195.47";
+    }
+    else if (choice == "2")
+    {
+        ip = "10.35.195.48";
+    }
+    else if (choice == "3")
+    {
+        ip = "10.35.195.49";
+    }
+    else if (choice == "4")
+    {
+        ip = "127.0.0.1";
+    }
+    // assign IP, PORT
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr(ip.c_str());
+    servaddr.sin_port = htons(PORT);
+
+    // connect the client socket to server socket
+    if (connect(sockfd, (SA *)&servaddr, sizeof(servaddr)) != 0)
+    {
+        printf("connection with the server failed...\n");
+        exit(0);
+    }
+    else
+        printf("connected to the server..\n");
+
+    // function for chat
+    func(sockfd);
+
+    // close the socket
+    close(sockfd);
+}
---
+++ server.cpp
@@ -0,0 +1,95 @@
+// Write CPP code here
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <stdio.h>      /* for printf() and fprintf() */
+#include <sys/types.h>  /* for Socket data types */
+#include <sys/socket.h> /* for socket(), connect(), send(), and recv() */
+#include <netinet/in.h> /* for IP Socket data types */
+#include <arpa/inet.h>  /* for sockaddr_in and inet_addr() */
+#include <stdlib.h>     /* for atoi() */
+#include <string.h>     /* for memset() */
+#include <unistd.h>     /* for close() */
+#include <vector>
+#include <iostream>
+#define MAX 80
+#define PORT 9105
+#define SA struct sockaddr
+
+using namespace std;
+
+// Function designed for chat between client and server.
+void func(int sockfd)
+{
+    //create char array buffer
+    char *testBuff = new char[4096];
+    int testLength;
+    //receive length
+    recv(sockfd, &testLength, sizeof(testLength), 0);
+    //receive message
+    recv(sockfd, testBuff, testLength, 0);
+    string test = testBuff;
+
+    cout << "Received: " << test << endl;
+}
+
+// Driver function
+int main()
+{
+    int sockfd, connfd;
+    struct sockaddr_in servaddr, cli;
+
+    // socket create and verification
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1)
+    {
+        printf("socket creation failed...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully created..\n");
+    bzero(&servaddr, sizeof(servaddr));
+
+    // assign IP, PORT
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    //servaddr.sin_addr.s_addr = htonl("192.168.56.1");
+    servaddr.sin_port = htons(PORT);
+
+    // Binding newly created socket to given IP and verification
+    if ((bind(sockfd, (SA *)&servaddr, sizeof(servaddr))) != 0)
+    {
+        printf("socket bind failed...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully binded..\n");
+
+    // Now server is ready to listen and verification
+    if ((listen(sockfd, 5)) != 0)
+    {
+        printf("Listen failed...\n");
+        exit(0);
+    }
+    else
+        printf("Server listening..\n");
+    socklen_t len = sizeof(cli);
+
+    // Accept the data packet from client and verification
+    connfd = accept(sockfd, (SA *)&cli, &len);
+    if (connfd < 0)
+    {
+        printf("server acccept failed...\n");
+        exit(0);
+    }
+    else
+        printf("server acccept the client...\n");
+
+    // Function for chatting between client and server
+    func(connfd);
+
+    // After chatting close the socket
+    close(sockfd);
+}
